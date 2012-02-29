b8f3a0b7a2d153e9bede248ec4d23b74f7bb34d3
dan wager <dan_wager@medigap360.com>
2012-02-28 20:16:30 -0600

    added created alongside id

--- dtos/Dto.php
+++ dtos/Dto.php
@@ -3,12 +3,17 @@ namespace devmo\dtos;
 
 abstract class Dto extends \devmo\libs\Box {
 	protected $id;
-
+	protected $created;
 	public function setId ($id) {
 		return ($this->id = $id);
 	}
-
 	public function getId () {
 		return $this->id;
+	}
+	public function setCreated ($created) {
+		$this->created = $created;
+	}
+	public function getCreated () {
+		return $this->created;
 	}
 }
