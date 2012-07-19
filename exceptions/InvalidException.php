783da675dc1ffdb21e47fdba24b75b52721d789e
dan wager <dan_wager@medigap360.com>
2012-07-19 12:29:28 -0500

    removing debug in invalid exception when not debugging. moved Ajax view to Echo.

--- exceptions/InvalidException.php
+++ exceptions/InvalidException.php
@@ -6,7 +6,16 @@ class InvalidException extends \devmo\exceptions\Exception {
   public function __construct ($field, $value=null, $label=null) {
     $this->field = $field;
     $this->label = $label ?: ucfirst(preg_replace(array('/([A-Z]{1})/','/ Id/'),array(' \1',''),$field));
-    parent::__construct(($value ? "Invalid {$this->label}".(\devmo\Config::isDebug()?" [{$value}]":null) : "Missing {$this->label}")." ({$this->file}:{$this->line})");
+		$debug = "";
+		if (\devmo\Config::isDebug()) {
+			$debug = ' [';
+			ob_start();
+			var_dump($value);
+			$debug .= ob_get_contents();
+			ob_end_clean();
+			$debug .= "] ({$this->file}:{$this->line})";
+		}
+    parent::__construct(($value ? "Invalid {$this->label}" : "Missing {$this->label}").$debug);
   }
 	public function getField () {
 		return $this->field;
---
+++ views/Echo.php
@@ -0,0 +1,1 @@
+<?=$this->get('string')?>
