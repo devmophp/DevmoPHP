45566d088376d73501952b31429b21c18c311fe7
danit <dan.wager@gmail.com>
2012-12-04 09:48:05 -0600

    fix for ClassNotFoundView calling undefined method.

--- views/ClassNotFoundError.php
+++ views/ClassNotFoundError.php
@@ -1,4 +1,4 @@
 <h2>Class Not Found</h2>
-<label>class:</label><?=$this->get('class')?>
+<label>class:</label><?=$this->class?>
 <br />
-<label>file:</label><?=$this->get('file')?>
+<label>file:</label><?=$this->file?>
